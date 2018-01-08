fa17f3141b0ab86fca4c97a11df51ddb8caedbf6
ArtyProg <salvatore.didio@gmail.com>
2018-01-08 10:19:19 +0100

    Initial commit

---
+++ src/components/lessons/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+	export default {
+  name: 'hello-world',
+  data () {
+    return {
+      msg: 'Lesson One'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
